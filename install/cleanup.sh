f3a02701464c00853ff270c63995297958e8d53b
Jack Carnes <carnesj@us.ibm.com>
2021-10-01 14:29:37 -0500

    Update cleanup.sh

--- install/cleanup.sh
+++ install/cleanup.sh
@@ -4,6 +4,8 @@
 export TARGET_NAMESPACE=cp4i-mq
 export QMname=mq00pl
 export LABEL="tekton.dev/task=deploy-qm"
+export PL_LABEL="tekton.dev/pipeline=mq-pipeline"
+export TASK_LABEL="tekton.dev/pipelineTask=deploy-qm"
 
 oc delete queuemanager $QMname -n $TARGET_NAMESPACE
 oc delete secret user-at-github -n $TARGET_NAMESPACE
@@ -12,3 +14,5 @@ oc delete task deploy-qm -n $TARGET_NAMESPACE
 oc delete eventlistener el-cicd-mq -n $TARGET_NAMESPACE
 oc delete pipeline mq-pipeline -n $TARGET_NAMESPACE
 oc delete pod -l $LABEL -n $TARGET_NAMESPACE
+oc delete pipelineRun -l $PL_LABEL -n $TARGET_NAMESPACE
+oc delete pipelineTask -l $TASK_LABEL -n $TARGET_NAMESPACE
